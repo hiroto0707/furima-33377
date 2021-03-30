ee9beb3e32584f63b1766812c610c9f20c690e64
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-03-30 13:24:39 +0900

    修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,11 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 0, category: " -------- " },
-    { id: 1, category: "レディース" },
-    { id: 2, category:   "メンズ"   },
-    { id: 3, category:   "キッズ・ベイビー"   },
-    { id: 4, category:   "トップス"   },
-    { id: 5, category:   "パンツ"   }
-    
+    { id: 0, category: " ------------------ " },
+    { id: 1, category:   "レディース"         },
+    { id: 2, category:   "メンズ"             },
+    { id: 3, category:   "キッズ"             },
+    { id: 4, category:   "トップス"           },
+    { id: 5, category:   "パンツ"             }
   ] 
 
   include ActiveHash::Associations
