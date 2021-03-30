95c7d7d063c1aaf2ff22c4cef29d91255a7812d1
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-03-30 14:20:38 +0900

    カテゴリー追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,18 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 0, category: " ------------------ " },
-    { id: 1, category:   "レディース"         },
-    { id: 2, category:   "メンズ"             },
-    { id: 3, category:   "キッズ"             },
-    { id: 4, category:   "トップス"           },
-    { id: 5, category:   "パンツ"             }
-  ] 
+    { id: 0, category: " ------------------------ " },
+    { id: 1, category:   "レディース"               },
+    { id: 2, category:   "メンズ"                   },
+    { id: 3, category:   "キッズ・ベビー"           },
+    { id: 4, category:   "インテリア・住まい・小物" },
+    { id: 6, category:   "本・音楽・ゲーム"         },
+    { id: 7, category:   "おもちゃ・グッツ"         },
+    { id: 8, category:   "家電・スマホ・カメラ"     },
+    { id: 9, category:   "スポーツ・レジャー"       },
+    { id: 10, category:  "その他"                   }
+  ]  
 
   include ActiveHash::Associations
   has_many :items
+  
 end
