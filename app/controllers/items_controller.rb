f786117bb56a42c8765d65f1a25a92fdab6597ba
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-01 15:59:02 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, expect: [:index]
 
   def index
-    @items =  Item.all
+    @items =  Item.order("name DESC")
   end
 
   def new
