a93cb77470d29d48c3bffd7fc22ab7635a92438a
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-03 15:15:23 +0900

    Revert "出品者削除"
    This reverts commit 3f0202b553d32522050cf223318f6b6585e2fe8f.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,7 @@ class ItemsController < ApplicationController
  end
 
  def destroy
-  if item.user_id == current_user.id
-    @titem.destroy
+  if @item.destroy
     redirect_to root_path
   end  
  end
