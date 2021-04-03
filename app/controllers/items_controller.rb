66f0644abe9e5144b0b61a1465604a51c3092d39
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-03 15:36:13 +0900

    メソッド名の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :before_action, only: [:edit, :update, :destroy]
+  before_action :move_to_root_path, only: [:edit, :update, :destroy]
 
 
   def index
