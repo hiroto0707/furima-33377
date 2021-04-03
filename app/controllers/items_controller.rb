9ccbc4c51c7da41c031dfb624f31c516f6c92289
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-03 14:31:14 +0900

    削除のリンク設定・アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :before_action, only: [:edit, :update]
 
 
@@ -35,13 +35,20 @@ class ItemsController < ApplicationController
   end  
  end
 
+ def destroy
+  if @item.destroy
+    redirect_to root_path
+  end  
+ end
+
+ 
+
  private
  def move_to_root_path
   unless current_user.id == @item.user_id
     redirect_to root_path
   end  
-end
-
+ end
 
  def set_item
   @item = Item.find(params[:id])
