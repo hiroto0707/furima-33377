78ee1e65d898a32f0a3cc68268ef8fa87de93636
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-07 16:01:16 +0900

    Formオブジェクトテストコード

--- app/models/user_donation.rb
+++ app/models/user_donation.rb
@@ -1,19 +1,16 @@
 class UserDonation
   include ActiveModel::Model
-  attr_accessor :area_id,:municipalities,:address,:building_number,:postal_code,:phone_number,:user_id,:item_id
+  attr_accessor :area_id,:municipalities,:address,:building_number,:postal_code,:phone_number,:user_id,:item_id,:order_id
 
    validates :area_id,numericality: { other_than: 0 }
    validates :municipalities,presence: true
    validates :address,presence: true
    validates :postal_code,presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
-
-   with_options presence: true, format: { with: /\A\d{11}\z/ } do
-    validates :building_number
-    validates :phone_number
-   end
+   validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }
+  
 
   def save
-    Order.create(user_id: user_id, item_id: item_id)
-    Place.create(municipalities: municipalities, address: address, building_number: building_number,postal_code: postal_code,phone_number: phone_number,area_id: area_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Place.create(municipalities: municipalities, address: address, building_number: building_number,postal_code: postal_code,phone_number: phone_number,area_id: area_id, order_id: order.id)
   end
 end
---
+++ spec/models/user_donation_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe UserDonation, type: :model do
+  before do
+    @order = FactoryBot.build(:user_donation)
+  end
+  
+  describe '商品購入登録' do
+    context '商品購入登録ができる時' do
+      it 'area_id,municipalities,address,postal_code,phone_numberが存在すれば登録できる' do
+      expect(@order).to be_valid
+      end
+
+      it 'building_numberが空でも登録できる' do
+        expect(@order).to be_valid
+      end
+    end  
+
+    context '商品購入登録ができない時' do
+      it 'area_idが空では登録できない' do
+        @order.area_id = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Area is not a number")
+      end
+
+      it 'area_idが0の場合出品できない' do
+        @order.area_id = 0
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Area must be other than 0")
+      end
+
+      it 'municipalitiesが空では登録できない' do
+        @order.municipalities = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Municipalities can't be blank")
+      end
+
+      it 'addressが空では登録できない' do
+        @order.address = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Address can't be blank")
+      end
+
+      it 'postal_codeが空では登録できない' do
+        @order.postal_code = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid")
+      end
+
+      it 'postal_codeは全角では登録できない' do
+        @order.postal_code = "３０００"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid")   
+      end
+
+      it 'postal_codeは半角英数混合では登録できない' do
+        @order.postal_code = 'a1'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid")
+      end
+
+      it 'phone_numberが空では登録できない' do
+        @order.phone_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'phone_numberは全角では登録できない' do
+        @order.phone_number = "３０００"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid")   
+      end
+
+      it 'phone_numberは半角英数混合では登録できない' do
+        @order.phone_number = 'a1'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid")
+    end
+    end
+  end
+end
