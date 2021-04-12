ac228fcf95959b65c036c70cf570bdc142e3eaa2
hiroto0707 <77100499+hiroto0707@users.noreply.github.com>
2021-04-12 18:41:14 +0900

    価格制限変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,5 +30,5 @@ class Item < ApplicationRecord
   end
 
  validates :price, presence: true, numericality: { only_integer: true}
- validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000, message: "is out of setting range" }
+ validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 10000000, message: "is out of setting range" }
 end
